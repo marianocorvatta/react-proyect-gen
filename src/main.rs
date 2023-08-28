1906492920f90def36ed9f9a32560d63127c6c36
Mariano Corvatta <62441854+marianocorvatta@users.noreply.github.com>
2023-08-28 16:53:15 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ struct Proyect {
 fn main() -> std::io::Result<()> {
     println!(); 
     println!("{}", "***************".bold().yellow());
-    println!("{}", "Hello from react-proyect-gen !".bold().yellow());
+    println!("{}", "Hello from React Proyect Generator !".bold().yellow());
     println!("{}", "***************".bold().yellow());
     println!();
     println!("This program will create a new react proyect.");
@@ -26,7 +26,7 @@ fn main() -> std::io::Result<()> {
         .read_line(&mut proyect_name)
         .expect("Failed to read line");
     
-    let projects: Vec<Proyect> = vec![
+    let templates: Vec<Proyect> = vec![
         Proyect {
             name: String::from("nextjs"),
             command: String::from("npx"),
@@ -64,269 +64,87 @@ fn main() -> std::io::Result<()> {
         },
     ];
     
-    let mut proyect_selected = String::new();
+    let mut template_selected = String::new();
 
     loop {
         println!();
         println!("Select the proyect type:");
 
-        for (index, project) in projects.iter().enumerate() {
+        for (index, project) in templates.iter().enumerate() {
             println!("{}: {}", index + 1, project.name);
         }    
         println!();
 
         io::stdin()
-            .read_line(&mut proyect_selected)
+            .read_line(&mut template_selected)
             .expect("Failed to read line");
 
-        proyect_selected = proyect_selected.trim().to_string();
+        template_selected = template_selected.trim().to_string();
 
-        if proyect_selected == "exit" {
+        if template_selected == "exit" {
             println!();
             println!("Bye!");
             break;
         }
  
-        if proyect_selected == "1" || proyect_selected == "nextjs" {
-            println!();
-            println!("You selected: nextjs !");
-            let confirm = confirm_action();
-
-            if confirm {
-                println!();
-                println!("Starting process...");
-                println!();
-
-                let mut cmd = Command::new("npx")
-                    .arg("create-next-app@latest")
-                    .arg(&proyect_name)
-                    .stdin(Stdio::inherit())
-                    .stdout(Stdio::inherit())
-                    .stderr(Stdio::inherit())
-                    .spawn()
-                    .expect("failed to execute process");
-
-                let status = cmd.wait().expect("failed to wait for child process");
-                
-                if status.success() {
-                    println!("{}", "Proyect created !".green().bold());
-                    println!();
-                    println!("To run the proyect:");
-                    println!("cd {}", proyect_name);
-                    println!("npm run dev");
-                } else {
-                    println!("{}", "Command failed".red().bold());
-                }
-            }
-            
+        if template_selected == "1" || template_selected == "nextjs" {
+            create_proyect(&templates[0].name, &templates[0].command, &templates[0].args, &proyect_name);
             break;
-        } else if proyect_selected == "2" || proyect_selected == "vitejs" {
-            println!();
-            println!("You selected: vitejs !");
-            let confirm = confirm_action();
-
-            if confirm {
-                println!();
-                println!("Starting process...");
-                println!();
-
-                let mut cmd = Command::new("npm")
-                    .arg("create")
-                    .arg("vite@latest")
-                    .arg(&proyect_name)
-                    .stdin(Stdio::inherit())
-                    .stdout(Stdio::inherit())
-                    .stderr(Stdio::inherit())
-                    .spawn()
-                    .expect("failed to execute process");
-
-                let status = cmd.wait().expect("failed to wait for child process");
-                
-                if status.success() {
-                    println!("{}", "Proyect created !".green().bold());
-                    println!();
-                    println!("To run the proyect:");
-                    println!("cd {}", proyect_name);
-                    println!("npm install");
-                    println!("npm run dev");
-                } else {
-                    println!("{}", "Command failed".red().bold());
-                }
-            }
-           
+        } else if template_selected == "2" || template_selected == "vitejs" {
+            create_proyect(&templates[1].name, &templates[1].command, &templates[1].args, &proyect_name);
             break;
-        } else if proyect_selected == "3" || proyect_selected == "t3-app" {    
-            println!();
-            println!("You selected: t3-app !");
-            let confirm = confirm_action();
-
-            if confirm {
-                println!();
-                println!("Starting process...");
-                println!();
-
-                let mut cmd = Command::new("npm")
-                    .arg("create")
-                    .arg("t3-app@latest")
-                    .arg(&proyect_name)
-                    .stdin(Stdio::inherit())
-                    .stdout(Stdio::inherit())
-                    .stderr(Stdio::inherit())
-                    .spawn()
-                    .expect("failed to execute process");
-
-                let status = cmd.wait().expect("failed to wait for child process");
-                
-                if status.success() {
-                    println!("{}", "Proyect created !".green().bold());
-                    println!();
-                    println!("To run the proyect:");
-                    println!("cd {}", proyect_name);
-                    println!("npm start");
-                } else {
-                    println!("{}", "Command failed".red().bold());
-                }
-            }
-
+        } else if template_selected == "3" || template_selected == "t3-app" {    
+            create_proyect(&templates[2].name, &templates[2].command, &templates[2].args, &proyect_name);
             break;
-        } else if proyect_selected == "4" || proyect_selected == "cra" || proyect_selected == "create-react-app" {
-            println!();
-            println!("You selected: create-react-app !");
-            let confirm = confirm_action();
-
-            if confirm {
-                println!();
-                println!("Starting process...");
-                println!();
-
-                let mut cmd = Command::new("npx")
-                    .arg("create-react-app")
-                    .arg(&proyect_name)
-                    .stdin(Stdio::inherit())
-                    .stdout(Stdio::inherit())
-                    .stderr(Stdio::inherit())
-                    .spawn()
-                    .expect("failed to execute process");
-
-                let status = cmd.wait().expect("failed to wait for child process");
-                
-                if status.success() {
-                    println!("{}", "Proyect created !".green().bold());
-                    println!();
-                    println!("To run the proyect:");
-                    println!("cd {}", proyect_name);
-                    println!("npm start");
-                } else {
-                    println!("{}", "Command failed".red().bold());
-                }
-            }
-
+        } else if template_selected == "4" || template_selected == "cra" || template_selected == "create-react-app" {
+            create_proyect(&templates[3].name, &templates[3].command, &templates[3].args, &proyect_name);
             break;
-        } else if proyect_selected == "5" || proyect_selected == "react-native" {
-            println!();
-            println!("You selected: react-native !");
-            let confirm = confirm_action();
-
-            if confirm {
-                println!();
-                println!("Starting process...");
-                println!();
-
-                let mut cmd = Command::new("npx")
-                    .arg("react-native@latest")
-                    .arg("init")
-                    .arg(&proyect_name)
-                    .stdin(Stdio::inherit())
-                    .stdout(Stdio::inherit())
-                    .stderr(Stdio::inherit())
-                    .spawn()
-                    .expect("failed to execute process");
-
-                let status = cmd.wait().expect("failed to wait for child process");
-                
-                if status.success() {
-                    println!("{}", "Proyect created !".green().bold());
-                    println!();
-                    println!("To run the proyect:");
-                    println!("cd {}", proyect_name);
-                    println!("npx expo start");
-                    println!("and follow the instructions in this link https://reactnative.dev/docs/environment-setup?guide=quickstart&os=windows&platform=android");
-                } else {
-                    println!("{}", "Command failed".red().bold());
-                }
-            }
-
+        } else if template_selected == "5" || template_selected == "react-native" {
+            create_proyect(&templates[4].name, &templates[4].command, &templates[4].args, &proyect_name);
             break;
-        } else if proyect_selected == "6" || proyect_selected == "create-expo-app" {
-            println!();
-            println!("You selected: create-expo-app !");
-            let confirm = confirm_action();
-
-            if confirm {
-                println!();
-                println!("Starting process...");
-                println!();
-
-                let mut cmd = Command::new("npx")
-                    .arg("create-expo-app")
-                    .arg(&proyect_name)
-                    .stdin(Stdio::inherit())
-                    .stdout(Stdio::inherit())
-                    .stderr(Stdio::inherit())
-                    .spawn()
-                    .expect("failed to execute process");
-
-                let status = cmd.wait().expect("failed to wait for child process");
-                
-                if status.success() {
-                    println!("{}", "Proyect created !".green().bold());
-                    println!();
-                    println!("To run the proyect:");
-                    println!("Follow the instructions in this link https://reactnative.dev/docs/environment-setup?guide=native");
-                } else {
-                    println!("{}", "Command failed".red().bold());
-                }
-            }
-
+        } else if template_selected == "6" || template_selected == "create-expo-app" {
+            create_proyect(&templates[5].name, &templates[5].command, &templates[5].args, &proyect_name);
             break;
-        } else if proyect_selected == "7" || proyect_selected == "turborepo" {
-            println!();
-            println!("You selected: Turborepo react-native starter !");
-            let confirm = confirm_action();
-
-            if confirm {
-                println!();
-                println!("Starting process...");
-                println!();
-
-                let mut cmd = Command::new("npx")
-                    .arg("create-turbo@latest")
-                    .arg("-e")
-                    .arg(&proyect_name)
-                    .stdin(Stdio::inherit())
-                    .stdout(Stdio::inherit())
-                    .stderr(Stdio::inherit())
-                    .spawn()
-                    .expect("failed to execute process");
-
-                let status = cmd.wait().expect("failed to wait for child process");
-                
-                if status.success() {
-                    println!("{}", "Proyect created !".green().bold());
-                    println!();
-                    println!("To run the proyect:");
-                    println!("Follow the instructions in this link https://reactnative.dev/docs/environment-setup?guide=native");
-                } else {
-                    println!("{}", "Command failed".red().bold());
-                }
-            }
-
+        } else if template_selected == "7" || template_selected == "turborepo" {
+            create_proyect(&templates[6].name, &templates[6].command, &templates[6].args, &proyect_name);
             break;
         } else {
             println!("{}", "Please select a valid option.".red().bold());
         }
     }
     Ok(())
+}
+
+fn create_proyect(name: &String, command: &String, args: &String, proyect_name: &String) -> bool {
+    println!();
+    println!("You selected: {} !", name);
+
+    let confirm = confirm_action();
+
+    println!();
+    println!("Starting process...");
+    println!();
+    
+    if confirm {
+        let mut cmd = Command::new(command)
+            .args(args.split_whitespace())
+            .arg(proyect_name)
+            .stdin(Stdio::inherit())
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .spawn()
+            .expect("failed to execute process");
+
+        let status = cmd.wait().expect("failed to wait for child process");
+        
+        if status.success() {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+       return false;
+    }
 }
 
 fn confirm_action() -> bool {
@@ -341,25 +159,6 @@ fn confirm_action() -> bool {
     confirm = confirm.trim().to_string();
 
     if confirm == "y" || confirm == "yes" {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-fn create_proyect (command: &String, args: &String, proyect_name: &String) -> bool {
-    let mut cmd = Command::new(command)
-        .arg(args)
-        .arg(proyect_name)
-        .stdin(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .spawn()
-        .expect("failed to execute process");
-
-    let status = cmd.wait().expect("failed to wait for child process");
-    
-    if status.success() {
         return true;
     } else {
         return false;
